11920c830f166a7fd414e5a6dea6dd1acc2912ee
c.bushehry <c.bushehry@gmail.com>
2023-09-26 19:58:02 -0700

    created preload.js v1

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,25 +12,6 @@ const KEY_CONFIG = {
     STRAFE_RIGHT: 'D'
 };
 
-function preload() {
-    this.load.image('background', 'assets/images/background1.png');
-    this.load.image('background-stars', 'assets/images/background2.png');
-    this.load.image('playerShip1', 'assets/images/playerShip1.png');
-    this.load.image('playerShip2', 'assets/images/playerShip2.png');
-    this.load.image('laser1', 'assets/images/laser1.png');
-    this.load.image('heart', 'assets/images/heart.png');
-
-    this.load.image('asteroid1', 'assets/images/asteroid1.png');
-    this.load.image('asteroid2', 'assets/images/asteroid2.png');
-    this.load.image('explosion1', 'assets/images/explosion/explosion1.png');
-    this.load.image('explosion2', 'assets/images/explosion/explosion2.png');
-    this.load.image('explosion3', 'assets/images/explosion/explosion3.png');
-    this.load.image('explosion4', 'assets/images/explosion/explosion4.png');
-    this.load.image('explosion5', 'assets/images/explosion/explosion5.png');
-    this.load.image('explosion6', 'assets/images/explosion/explosion6.png');
-    this.load.image('explosion7', 'assets/images/explosion/explosion7.png');
-}
-
 function create() {
     this.bg = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'background').setOrigin(0);
     this.bgStars = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, 'background-stars').setOrigin(0);
