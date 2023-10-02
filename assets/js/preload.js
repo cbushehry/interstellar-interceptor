bc9aa7931b5c86d818ad7a9a51885f051072822e
c.bushehry <c.bushehry@gmail.com>
2023-10-01 22:27:20 -0700

    added all util images to preload v2

--- assets/js/preload.js
+++ assets/js/preload.js
@@ -1,15 +1,26 @@
 function preload() {
-    this.load.image('background', 'assets/images/background1.png');
-    this.load.image('background-stars', 'assets/images/background2.png');
+    this.load.image('background', 'assets/images/background/background.png');
+    this.load.image('background-stars', 'assets/images/background/background-stars.png');
+    this.load.image('earth', 'assets/images/background/earth.png');
+    this.load.image('mars1', 'assets/images/background/mars1.png');
+    this.load.image('mars2', 'assets/images/background/mars2.png');
+    this.load.image('moon', 'assets/images/background/moon.png');
     
     this.load.image('playerShip1', 'assets/images/playerShip1.png');
     this.load.image('playerShip2', 'assets/images/playerShip2.png');
     this.load.image('heart', 'assets/images/heart.png');
+    this.load.image('shield', 'assets/images/shield.png');
 
     this.load.image('laser1', 'assets/images/lasers/laser1.png');
     this.load.image('laser2', 'assets/images/lasers/laser2.png');
     this.load.image('laser3', 'assets/images/lasers/laser3.png');
     this.load.image('laser4', 'assets/images/lasers/laser4.png');
+
+    this.load.image('powerup1', 'assets/images/lasers/powerup1.png'); // + LASER POWER
+    this.load.image('powerup2', 'assets/images/lasers/powerup2.png'); // + LASER SPEED
+    this.load.image('powerup3', 'assets/images/lasers/powerup3.png'); // + HEALTH
+    this.load.image('powerup4', 'assets/images/lasers/powerup4.png'); // + SHIELD
+    this.load.image('powerup5', 'assets/images/lasers/powerup5.png'); // + SPEED
 
     this.load.image('asteroid1', 'assets/images/asteroid1.png');
     this.load.image('asteroid2', 'assets/images/asteroid2.png');
